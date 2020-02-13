d9868d8dc40ec69c900ee3e994b3c4ac69374d2e
KanchanChaudhary <chaudharykanchan.21@gmail.com>
2020-02-13 13:26:46 +0545

    contact added

--- routes/contact.js
+++ routes/contact.js
@@ -1,5 +1,7 @@
 const app = module.exports = require('express')();
 const contactController = require('../controllers/contact')
 
+
+
 app.post('/', contactController.Addcontact);
 app.get('/', contactController.getAllContact);
